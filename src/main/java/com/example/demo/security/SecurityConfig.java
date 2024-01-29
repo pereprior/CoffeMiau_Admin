d5ef185245e54caa76a835bdea0566cae94633c3
pereprior <alupri5332@ieselcaminas.org>
2024-01-29 18:40:16 +0100

    Pere - SecurityConfig

---
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.example.demo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+
+        UserDetails clientTest = User.builder()
+                .username("client")
+                .password("{noop}hola01")
+                .roles("CLIENTE")
+                .build();
+
+        UserDetails employeeTest = User.builder()
+                .username("employee")
+                .password("{noop}hola01")
+                .roles("EMPLEADO")
+                .build();
+
+        UserDetails adminTest = User.builder()
+                .username("admin")
+                .password("{noop}hola01")
+                .roles("EMPLEADO","ADMINISTRADOR")
+                .build();
+
+        return new InMemoryUserDetailsManager(clientTest, employeeTest, adminTest);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers("/").hasRole("CLIENTE")
+                        .requestMatchers("/users/**").hasRole("EMPLEADO")
+                        .requestMatchers("/systems/**").hasRole("ADMINISTRADOR")
+                        .anyRequest().authenticated()
+        ).formLogin(form ->
+                form
+                        .loginPage("/auth/login")
+                        .loginProcessingUrl("/authenticateUser")
+                        .permitAll()
+        ).logout(
+                LogoutConfigurer::permitAll
+        ).exceptionHandling(configurer ->
+                configurer.accessDeniedPage("/access-denied")
+        )
+        ;
+
+        return http.build();
+    }
+}
