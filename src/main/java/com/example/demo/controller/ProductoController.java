f43e683fdab494c99521c5390d494624d7b2a7d3
Denis <aluion9738@ieselcaminas.org>
2024-01-31 19:18:21 +0100

    .

--- src/main/java/com/example/demo/controller/ProductoController.java
+++ src/main/java/com/example/demo/controller/ProductoController.java
@@ -5,9 +5,12 @@ import com.example.demo.models.services.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
-@RestController
+@Controller
 public class ProductoController {
     @Autowired
     private ProductoService productoService;
