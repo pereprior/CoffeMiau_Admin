4faddf9600c04e15b0ab58174cfc2271eca3c340
pereprior <alupri5332@ieselcaminas.org>
2024-02-07 19:08:40 +0100

    Contraseña encriptada

--- src/main/java/com/example/demo/models/services/UsuarioService.java
+++ src/main/java/com/example/demo/models/services/UsuarioService.java
@@ -4,6 +4,7 @@ import com.example.demo.models.dao.IUsuarioDao;
 import com.example.demo.models.entities.Usuario;
 import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
@@ -14,15 +15,18 @@ import java.util.List;
 public class UsuarioService implements IUsuarioService {
 
     private final IUsuarioDao dao;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UsuarioService(IUsuarioDao dao) {
+    public UsuarioService(IUsuarioDao dao, PasswordEncoder passwordEncoder) {
         this.dao = dao;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     @Transactional
     public void save(Usuario usuario) {
+        usuario.setPassword(this.passwordEncoder.encode(usuario.getPassword()));
         dao.save(usuario);
     }
 
